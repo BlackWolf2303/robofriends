551d9a1b9dfe89ad42728dcd9765303d05420273
Hai.Ho <hai.ho@it-consultis.com>
2019-08-16 17:05:19 +0700

    update

---
+++ src/abc.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const FirstApp = () => {
+    return (<div className="container">
+        <h1 className="text-primary text-center"> this is first example</h1>
+    </div>);
+}
+
+export default FirstApp;
